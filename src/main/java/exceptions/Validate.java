f7138d6688704cf16d46b348e16e86b75c08aaf2
Poliakova75 <alla.poliakova75@gmail.com>
2023-11-16 13:46:38 +0500

    hwe3

--- src/main/java/exceptions/Validate.java
+++ src/main/java/exceptions/Validate.java
@@ -2,33 +2,43 @@ package exceptions;
 
 public class Validate {
     protected static void validateLoginAndPassword(String login, String password, String confirmPassword) {
-        String allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
 
-        try {
-            if (login.length() > 20 || containsOnlyAllowedChars(login, allowedChars)) {
-                throw new WrongLoginException("Длина логина превышает максимум");
-            }
 
-            if (password.length() > 20 || containsOnlyAllowedChars(password, allowedChars)) {
-                throw new WrongPasswordException("Длина пароля привышает максимум");
-            }
-
-            if (!password.equals(confirmPassword)) {
-                throw new WrongPasswordException("Пароли не совпадают");
-            }
-        } catch (WrongLoginException | WrongPasswordException e) {
-            System.out.println(e.getMessage());
-        }
     }
-    private static boolean containsOnlyAllowedChars(String input, String allowedChars) {
-        for (char c : input.toCharArray()) {
-            if (allowedChars.indexOf(c) == -1) {
-                return true;
+
+    private static boolean ValidUsername(String username) {
+        if (username.length() < 6 || username.length() > 20) {
+            return false;
+        }
+        if (!username.matches("[A-Za-z0-9_]+")) {
+            return false;
+        }
+
+        return Character.isLetterOrDigit(username.charAt(0));
+    }
+    public static boolean ValidPassword(String password) {
+        if (password.length() < 8 || password.length() > 20) {
+            return false;
+        }
+
+        if (!password.matches("[A-Za-z0-9 ]+")) {
+            return false;
+        }
+
+        boolean hasLetter = false;
+        boolean hasDigit = false;
+
+        for (char c : password.toCharArray()) {
+            if (Character.isLetter(c)) {
+                hasLetter = true;
+            } else if (Character.isDigit(c)) {
+                hasDigit = true;
             }
         }
-        return false;
+        return hasLetter && hasDigit;
     }
 }
+
 
 
 
